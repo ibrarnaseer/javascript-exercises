205e95bf4da90eff04fefe785df7ae1a2f342d26
Ibrar <naseer.ibrar@gmail.com>
2022-09-13 20:37:54 +0200

    Complete exercise. Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num, num2, num3, num4) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === num || array[i] === num2 || array[i] === num3 || array[i] === num4) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
+
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
