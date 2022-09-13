4b83bdc1504ead7b5296c7fad028e61029edd93b
Ibrar <naseer.ibrar@gmail.com>
2022-09-13 21:39:29 +0200

    Complete exercise and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(degreeF) {
+  let degree = (degreeF - 32) / 1.8;
+  let result = Math.round(degree * 10) / 10;
+  return result
 };
 
-const ctof = function() {
-
+const ctof = function(degreeC) {
+  let degree = (degreeC * 1.8) + 32;
+  let result = Math.round(degree * 10) / 10;
+  return result;
 };
+
+ftoc(-100);
+ctof(-10);
 
 // Do not edit below this line
 module.exports = {
